b375b7c8b569b5aff3fe32cc6d5d2c2acbf2bff2
geuddd <99445553+geuddd@users.noreply.github.com>
2024-05-14 09:16:25 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,8 @@
 const config = {
-  challenge: true,
+  challenge: false, // Set to true if you want to enable password protection.
   users: {
-    // username: 'password', you can add multiple users.
-    moltybread: 'poopoo23',
-    tortapounder69: 'ilovetortas',
-    monkeyboy08: '*****-',
-    mason: 'orangehair12',
-    bigcashypoo: 'bakedbeans69',
-    Whipzyxtreamyt: 'breadandbeans87',
-    nyck: 'Basketball3!',
-    geanbean: 'ginob_24',
-    emmett_dobroth: '5Whitney1!',
-    Mason_games: 'MasonG2009',
-    Bigdawg53: 'hood99',
-    joshwillis: 'Walnut733',
-    samuelbikle: '02/19/09',
-    ant: '01/28/09'
-    
-  }
+    // You can add multiple users by doing username: 'password'.
+    interstellar: "password",
+  },
 }
 export default config
